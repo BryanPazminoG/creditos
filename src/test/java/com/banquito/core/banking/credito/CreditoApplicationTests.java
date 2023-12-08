03628bc001a77f36fb7634f373bdb594f747eb7b
Discat2 <ragarcia12@espe.edu.ec>
2023-12-08 15:23:18 -0500

    Proyecto

---
+++ src/test/java/com/banquito/core/banking/credito/CreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.banking.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
